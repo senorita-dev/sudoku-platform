2abc89d3a04c6c0557b96e48a0f3ea9477510e41
Ren Saito <ren.saito.dev@gmail.com>
2024-10-14 00:01:14 +1300

    prettier fix

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,3 @@
 export type Cell = number | number[] | null
 export type Grid = Cell[][]
-export type CellPosition = { row: number, col: number }
+export type CellPosition = { row: number; col: number }
