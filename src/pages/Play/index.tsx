f154de40226ead2e94453531bad4e18e499eac84
Ren Saito <ren.saito.dev@gmail.com>
2024-10-20 18:12:18 +1300

    sudoku page create responsive three-column layout

--- src/pages/Play/index.tsx
+++ src/pages/Play/index.tsx
@@ -1,33 +1,47 @@
+import SudokuControls from '@/components/sudoku/SudokuControls'
 import SudokuGrid from '@/components/sudoku/SudokuGrid'
-import { useSudokuActions, useSudokuDifficulty } from '@/hooks/useSudokuStore'
+import { useSudokuDifficulty } from '@/hooks/useSudokuStore'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Play() {
-  const dispatch = useSudokuActions()
   const difficulty = useSudokuDifficulty()
 
   return (
-    <div className="flex flex-grow flex-col items-center p-2">
-      <h1 className="text-4xl font-bold">Play</h1>
-      <Link to="/">Home</Link>
-      <div className="flex gap-4">
-        <button onClick={() => dispatch({ type: 'LOAD', payload: null })}>load</button>
-        <button onClick={() => dispatch({ type: 'LOAD', payload: 'easy' })}>easy</button>
-        <button onClick={() => dispatch({ type: 'LOAD', payload: 'medium' })}>medium</button>
-        <button onClick={() => dispatch({ type: 'LOAD', payload: 'hard' })}>hard</button>
-        <button onClick={() => dispatch({ type: 'LOAD', payload: 'expert' })}>expert</button>
-        <button onClick={() => dispatch({ type: 'CLEAR' })}>clear</button>
-        <button onClick={() => dispatch({ type: 'RESET' })}>reset</button>
-        <button onClick={() => dispatch({ type: 'SET_EDIT_MODE', payload: 'normal' })}>edit normal mode</button>
-        <button onClick={() => dispatch({ type: 'SET_EDIT_MODE', payload: 'pencil' })}>edit pencil mode</button>
+    <div className="flex flex-grow flex-col">
+      <header className="text-center">
+        <h1 className="text-4xl font-bold">Play</h1>
+        <Link to="/">Home</Link>
+      </header>
+      <div className="flex flex-grow">
+        <Left />
+        <Main>
+          <SudokuGrid />
+        </Main>
+        <Right>
+          <SudokuControls />
+        </Right>
       </div>
-      <div className="h-[min(80vw,80vh)] w-[min(80vw,80vh)] bg-white">
-        <SudokuGrid />
-      </div>
-      <div>
-        <span>{difficulty && <>difficulty: {difficulty}</>} </span>
-      </div>
+      <footer className="py-4 text-center">
+        <span>{difficulty && <>Difficulty: {difficulty}</>}</span>
+      </footer>
     </div>
+  )
+}
+
+function Left() {
+  return <aside className="w-full p-4 md:w-1/4" />
+}
+
+function Right({ children }: { children: React.ReactNode }) {
+  return <aside className="w-full p-4 md:w-1/4">{children}</aside>
+}
+
+function Main({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="flex flex-grow items-center justify-center p-4">
+      <div className="h-[min(80vw,80vh)] w-[min(80vw,80vh)] bg-white">{children}</div>
+    </main>
   )
 }
 
